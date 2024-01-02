73a29e1b2cdbbcb47e8f6c07836948f0b83be3b6
spoock1024 <me@spoock.com>
2024-01-02 21:52:57 +0800

    Add eBPF program for packet classification and processing

---
+++ bpf_tail_call/go.mod
@@ -0,0 +1,23 @@
+module bpf_tail_call
+
+go 1.20
+
+require github.com/gojue/ebpfmanager v0.4.4
+
+require (
+	github.com/avast/retry-go v3.0.0+incompatible // indirect
+	github.com/cilium/ebpf v0.12.3 // indirect
+	github.com/florianl/go-tc v0.4.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/josharian/native v0.0.0-20200817173448-b6b71def0850 // indirect
+	github.com/mdlayher/netlink v1.4.1 // indirect
+	github.com/mdlayher/socket v0.0.0-20210307095302-262dc9984e00 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/vishvananda/netlink v1.1.0 // indirect
+	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
+	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
+	golang.org/x/net v0.0.0-20210610124326-52da8fb2a613 // indirect
+	golang.org/x/sys v0.14.1-0.20231108175955-e4099bfacb8c // indirect
+)
---
+++ bpf_tail_call/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/link"
+	"log"
+	"os"
+	"os/signal"
+)
+
+//go:embed ebpf/bin/probe.o
+var _bytecode []byte
+
+func main() {
+	specs, err := ebpf.LoadCollectionSpecFromReader(bytes.NewReader(_bytecode))
+	if err != nil {
+		log.Fatalf("loading collection spec: %v", err)
+	}
+
+	coll, err := ebpf.NewCollection(specs)
+	if err != nil {
+		log.Fatalf("creating collection: %v", err)
+	}
+	defer coll.Close()
+
+	// Load XDP programs from the collection
+	icmpProg := coll.Programs["handle_icmp"]
+	tcpProg := coll.Programs["handle_tcp"]
+	udpProg := coll.Programs["handle_udp"]
+	//
+	// Get the packet_processing_progs map
+	packetProgsMap := coll.Maps["packet_processing_progs"]
+
+	// Update map entries with the program file descriptors
+	keysAndProgs := map[uint32]*ebpf.Program{
+		0: icmpProg,
+		1: tcpProg,
+		2: udpProg,
+	}
+
+	for key, prog := range keysAndProgs {
+		if prog == nil {
+			log.Fatalf("program with key %d not found", key)
+		}
+		fmt.Println(fmt.Sprintf("key: %d, prog: %v", key, prog))
+		if err := packetProgsMap.Update(key, uint32(prog.FD()), ebpf.UpdateAny); err != nil {
+			log.Fatalf("updating packet_processing_progs map: %v", err)
+		}
+	}
+
+	// Attach the XDP program to the interface
+	xdpProg := coll.Programs["packet_classifier"]
+	xdpLink, err := link.AttachXDP(link.XDPOptions{
+		Program:   xdpProg,
+		Interface: 2,
+	})
+	if err != nil {
+		log.Fatalf("attaching xdp: %v", err)
+	}
+	defer xdpLink.Close()
+
+	log.Println("eBPF program successfully attached. Press CTRL+C to stop.")
+
+	// Wait for the user to press CTRL+C
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	<-sig
+
+	log.Println("Detaching program and closing link")
+}
